43f578969a3c3f92622fe2b3c116a149fff5f5b8
zihe <he100zihao100@126.com>
2020-12-08 15:57:14 +0800

    update: 完成-17_插件

--- vue_test/13_插件/vue-myPlugin.js
+++ vue_test/13_插件/vue-myPlugin.js
@@ -1,20 +1,26 @@
+/**
+ * Vue的插件库
+ */
 (function (window) {
-  const MyPlugin = {}
-  MyPlugin.install = function (Vue, options) {
-    // 1. 添加全局方法或属性
-    Vue.myGlobalMethod = function () {
-      console.log('Vue函数对象的myGlobalMethod()')
-    }
+    //需要向外暴露的插件对象
+    const MyPlugin = {};
+    //插件必须要有一个install方法
+    MyPlugin.install = function (Vue, options) {
+        //1.添加全局方法或属性
+        Vue.myGlobalMethod = function () {
+            console.log('Vue函数对象的myGlobalMethod()')
+        };
 
-    // 2. 添加全局资源
-    Vue.directive('my-directive',function (el, binding) {
-      el.textContent = 'my-directive----'+binding.value
-    })
+        //2.添加全局资源
+        Vue.directive('my-directive', function (el, binding) {
+            el.textContent = binding.value.toUpperCase();
+        });
 
-    // 4. 添加实例方法
-    Vue.prototype.$myMethod = function () {
-      console.log('vm $myMethod()')
-    }
-  }
-  window.MyPlugin = MyPlugin
-})(window)
+        //4.添加实例方法，一般都有$前缀
+        Vue.prototype.$myMethod = function () {
+            console.log('Vue实例对象的$myMethod()');
+        };
+    };
+    //重点，向外暴露插件对象，外部才能获取到插件对象
+    window.MyPlugin = MyPlugin
+})(window);
