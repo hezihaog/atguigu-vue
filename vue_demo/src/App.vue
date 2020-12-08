917ccbf9028e9bae21ad9ffaf0f6387f427f8228
zihe <he100zihao100@126.com>
2020-12-08 17:38:37 +0800

    add: 增加一个评论板的小例子

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <!-- :addComment，将定义的增加评论的方法传递给子组件使用 -->
+      <Add :addComment="addComment"/>
+      <!-- :comments，将数据通过属性传递到子组件 -->
+      <List :comments="comments" :deleteComment="deleteComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  //引入子组件
+  import Add from './components/Add'
+  import List from './components/List'
+
+  export default {
+    data() {
+      return {
+        comments: [//数据在哪个组件，更新数据的行为（方法）就应该定义在那个组件
+          {
+            name: 'Bom',
+            content: 'Vue 还不错'
+          }, {
+            name: 'Cat',
+            content: 'Vue So Easy'
+          }, {
+            name: 'BZ',
+            content: 'Vue So So'
+          }
+        ]
+      }
+    },
+    methods: {
+      //添加评论
+      addComment(comment) {
+        //将新评论插入到最前面
+        this.comments.unshift(comment);
+      },
+      //删除指定下标的评论
+      deleteComment(index) {
+        this.comments.splice(index, 1);
+      }
+    },
+    //将组件映射成标签
+    components: {
+      Add,
+      List
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_demo/src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 v-show="comments.length === 0">暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <!-- :comment，将评论数据传递到子组件中 -->
+      <!-- :deleteComment，将删除评论的方法传递到子组件中 -->
+      <!-- :index，将当前条目的下标传递到子组件中 -->
+      <Item v-for="(comment, index) in comments" :key="index"
+            :comment="comment" :deleteComment="deleteComment" :index="index"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+  //引入子组件
+  import Item from './Item'
+
+  export default {
+    //声明接收属性，这个属性就会成为组件对象的属性
+    props: [
+      'comments',
+      'deleteComment'
+    ],
+    //将组件映射成标签
+    components: {
+      Item
+    }
+  }
+</script>
+
+<style>
+  .reply {
+    margin-top: 0px;
+  }
+</style>
