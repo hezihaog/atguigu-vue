0b1285d2558ae7a5a810396ce37086460683ce9f
zihe <he100zihao100@126.com>
2020-12-09 17:44:32 +0800

    add: 增加vue-router编程式路由跳转练习

--- vue_demo/src/views/Message.vue
+++ vue_demo/src/views/Message.vue
@@ -5,8 +5,11 @@
       <li v-for="message in messages" :key="id">
         <!-- 点击后，跳转到详情，:to=``，路由地址动态变化，每个消息的id都不一样 -->
         <router-link :to="`/home/message/detail/${message.id}`">{{message.title}}</router-link>
+        <button @click="pushShow(message.id)">push查看</button>
+        <button @click="replaceShow(message.id)">replace查看</button>
       </li>
     </ul>
+    <button @click="$router.back()">回退</button>
     <hr>
     <!-- 消息详情 -->
     <router-view></router-view>
@@ -18,6 +21,16 @@
     data() {
       return {
         messages: []
+      }
+    },
+    methods: {
+      pushShow(id) {
+        //编程式路由，通过this.$router获取路由器，push()，压入一个MessageDetail消息详情组件
+        this.$router.push(`/home/message/detail/${id}`);
+      },
+      replaceShow(id) {
+        //replace()，替换一个组件
+        this.$router.replace(`/home/message/detail/${id}`);
       }
     },
     mounted() {
