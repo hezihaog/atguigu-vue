1c4884a7857224cea7e975017b2a78b8fdb2ba92
zihe <he100zihao100@126.com>
2020-12-09 15:22:59 +0800

    update: 将搜索Github用户的代码，单独放到一个src-users文件夹下

---
+++ vue_demo/src-users/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Search/>
+    <users-main/>
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search'
+  import Main from './components/Main'
+
+  export default {
+    components: {
+      Search,
+      //Main，在HTML中不能使用，所以单独给它起名字叫UsersMain
+      UsersMain: Main
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
