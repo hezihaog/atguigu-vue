b902dcf72c24936ae3b3ee5de7360cd082cc962d
zihe <he100zihao100@126.com>
2020-12-10 13:08:38 +0800

    update: 增加main.js中render函数的解释

--- vuex_test/src/main.js
+++ vuex_test/src/main.js
@@ -7,11 +7,30 @@ import store from "./store";
 //引入基础CSS样式
 import './base.css'
 
+//使用components和template，这2个属性，来指定根组件是App组件
+// new Vue({
+//   el: '#app',
+//   components: {
+//     App
+//   },
+//   template: '<App/>',
+//   store
+// });
+
+//使用render渲染函数，一个函数返回App组件，代替上面components和template，2个属性的功能
+// new Vue({
+//   el: '#app',
+//   render: function (createElement) {
+//     //相当于返回了<App/>组件
+//     //1句话，代替了上面的components和template
+//     return createElement(App);
+//   },
+//   store
+// });
+
 new Vue({
   el: '#app',
-  components: {
-    App
-  },
+  //箭头函数，简洁标签上面复杂的函数定义
   render: h => h(App),
   store
 });
